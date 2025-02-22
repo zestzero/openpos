94647bbe5fa13bdd9af220682900ca9e3e60c768
zestzero <krit.bannachaisirisuk@gmail.com>
2025-02-22 15:34:17 +0700

    feat(cross-platform): initiate cross-platform project

---
+++ cross-platform/src-tauri/src/lib.rs
@@ -0,0 +1,32 @@
+use tauri_plugin_sql::{Migration, MigrationKind};
+
+// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+#[tauri::command]
+fn greet(name: &str) -> String {
+    format!("Hello, {}! You've been greeted from Rust!", name)
+}
+
+#[cfg_attr(mobile, tauri::mobile_entry_point)]
+pub fn run() {
+    let migrations = vec![Migration {
+        version: 1,
+        description: "create users table",
+        sql: "CREATE TABLE IF NOT EXISTS users (  
+                id INTEGER PRIMARY KEY AUTOINCREMENT,  
+                name TEXT NOT NULL,  
+                email TEXT  
+            )",
+        kind: MigrationKind::Up,
+    }];
+
+    tauri::Builder::default()
+        .plugin(
+            tauri_plugin_sql::Builder::default()
+                .add_migrations("sqlite:database.db", migrations)
+                .build(),
+        )
+        .plugin(tauri_plugin_opener::init())
+        .invoke_handler(tauri::generate_handler![greet])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+}
---
+++ cross-platform/src/App.tsx
@@ -0,0 +1,112 @@
+import { useEffect, useState } from "react";
+import Database from "@tauri-apps/plugin-sql";
+import "./App.css";
+
+type User = {
+  id: number;
+  name: string;
+  email: string;
+};
+
+function App() {
+  const [isLoadingUsers, setIsLoadingUsers] = useState(true);
+  const [users, setUsers] = useState<User[]>([]);
+  const [name, setName] = useState<string>("");
+  const [email, setEmail] = useState<string>("");
+  const [error, setError] = useState<string>("");
+
+  async function getUsers() {
+    try {
+      const db = await Database.load("sqlite:test.db");
+      const dbUsers = await db.select<User[]>("SELECT * FROM users");
+
+      setError("");
+      setUsers(dbUsers);
+      setIsLoadingUsers(false);
+    } catch (error) {
+      console.log(error);
+      setError("Failed to get users - check console");
+    }
+  }
+
+  async function setUser(user: Omit<User, "id">) {
+    try {
+      setIsLoadingUsers(true);
+      const db = await Database.load("sqlite:database.db");
+
+      await db.execute("INSERT INTO users (name, email) VALUES ($1, $2)", [
+        user.name,
+        user.email,
+      ]);
+
+      getUsers().then(() => setIsLoadingUsers(false));
+    } catch (error) {
+      console.log(error);
+      setError("Failed to insert user - check console");
+    }
+  }
+
+  useEffect(() => {
+    getUsers();
+  }, []);
+
+  return (
+    <main className="container">
+      <h1>Welcome to Tauri + SQLite</h1>
+
+      {isLoadingUsers ? (
+        <div>Loading users...</div>
+      ) : (
+        <div style={{ display: "flex", flexDirection: "column", gap: "0.5rem" }}>
+          <form
+            className="row"
+            onSubmit={(e) => {
+              e.preventDefault();
+              setUser({ name, email });
+              getUsers();
+            }}
+          >
+            <input
+              id="name-input"
+              onChange={(e) => setName(e.currentTarget.value)}
+              placeholder="Enter a name..."
+            />
+            <input
+              type="email"
+              id="email-input"
+              onChange={(e) => setEmail(e.currentTarget.value)}
+              placeholder="Enter an email..."
+            />
+            <button type="submit">Add User</button>
+          </form>
+
+          <div style={{ display: "flex", flexDirection: "column", gap: "2rem" }}>
+            <h1>Users</h1>
+            <table>
+              <thead>
+                <tr>
+                  <th>ID</th>
+                  <th>Name</th>
+                  <th>Email</th>
+                </tr>
+              </thead>
+              <tbody>
+                {users.map((user) => (
+                  <tr key={user.id}>
+                    <td>{user.id}</td>
+                    <td>{user.name}</td>
+                    <td>{user.email}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </div>
+      )}
+
+      {error && <p>{error}</p>}
+    </main>
+  );
+}
+
+export default App;
