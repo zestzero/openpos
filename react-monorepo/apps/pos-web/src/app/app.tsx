3954ec8beb17b6712d4aeb86a7d75f67be4e92a6
zestzero <krit.bannachaisirisuk@gmail.com>
2025-03-08 19:59:28 +0700

    feat(react-monorepo): add react frontend

---
+++ react-monorepo/apps/pos-web/src/app/app.tsx
@@ -0,0 +1,13 @@
+import NxWelcome from './nx-welcome';
+import { Ui } from '@/shared/ui';
+
+export function App() {
+  return (
+    <div>
+      <Ui />
+      <NxWelcome title="pos-web" />
+    </div>
+  );
+}
+
+export default App;
