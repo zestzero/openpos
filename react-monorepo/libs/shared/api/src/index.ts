f0b622ca7dd9b1e52e72a4983260b2320d66e10f
zestzero <krit.bannachaisirisuk@gmail.com>
2025-05-31 14:54:47 +0700

    cleanup

--- react-monorepo/libs/shared/api/src/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './lib/context/ApiClientProvider'
-export * from './lib/context/apiClientContext'
-export * from './lib/hooks/useApiClient'
-export * from './lib/generated.d'
--- react-monorepo/libs/shared/api/src/lib/context/apiClientContext.ts
+++
@@ -1,7 +0,0 @@
-import { Client } from 'openapi-fetch'
-import { createContext } from 'react'
-import { paths } from '../generated'
-
-export const ApiClientContext = createContext<Client<paths> | undefined>(
-  undefined,
-)
