c4c7358b33b1adfa82c62390e1ecfbe46109cf38
zestzero <krit.bannachaisirisuk@gmail.com>
2025-05-31 15:09:45 +0700

    initiate new frontend

---
+++ frontend/src/main.tsx
@@ -0,0 +1,85 @@
+import { StrictMode } from 'react'
+import ReactDOM from 'react-dom/client'
+import {
+  Outlet,
+  RouterProvider,
+  createRootRoute,
+  createRoute,
+  createRouter,
+} from '@tanstack/react-router'
+import { TanStackRouterDevtools } from '@tanstack/react-router-devtools'
+import FormSimpleDemo from './routes/demo.form.simple.tsx'
+import FormAddressDemo from './routes/demo.form.address.tsx'
+import TableDemo from './routes/demo.table.tsx'
+import TanStackQueryDemo from './routes/demo.tanstack-query.tsx'
+
+import Header from './components/Header'
+
+import TanStackQueryLayout from './integrations/tanstack-query/layout.tsx'
+
+import * as TanStackQueryProvider from './integrations/tanstack-query/root-provider.tsx'
+
+import './styles.css'
+import reportWebVitals from './reportWebVitals.ts'
+
+import App from './App.tsx'
+
+const rootRoute = createRootRoute({
+  component: () => (
+    <>
+      <Header />
+      <Outlet />
+      <TanStackRouterDevtools />
+
+      <TanStackQueryLayout />
+    </>
+  ),
+})
+
+const indexRoute = createRoute({
+  getParentRoute: () => rootRoute,
+  path: '/',
+  component: App,
+})
+
+const routeTree = rootRoute.addChildren([
+  indexRoute,
+  FormSimpleDemo(rootRoute),
+  FormAddressDemo(rootRoute),
+  TableDemo(rootRoute),
+  TanStackQueryDemo(rootRoute),
+])
+
+const router = createRouter({
+  routeTree,
+  context: {
+    ...TanStackQueryProvider.getContext(),
+  },
+  defaultPreload: 'intent',
+  scrollRestoration: true,
+  defaultStructuralSharing: true,
+  defaultPreloadStaleTime: 0,
+})
+
+declare module '@tanstack/react-router' {
+  interface Register {
+    router: typeof router
+  }
+}
+
+const rootElement = document.getElementById('app')
+if (rootElement && !rootElement.innerHTML) {
+  const root = ReactDOM.createRoot(rootElement)
+  root.render(
+    <StrictMode>
+      <TanStackQueryProvider.Provider>
+        <RouterProvider router={router} />
+      </TanStackQueryProvider.Provider>
+    </StrictMode>,
+  )
+}
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals()
---
+++ frontend/vite.config.js
@@ -0,0 +1,19 @@
+import { defineConfig } from "vite";
+import viteReact from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+
+import { resolve } from "node:path";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [viteReact(), tailwindcss()],
+  test: {
+    globals: true,
+    environment: "jsdom",
+  },
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, './src'),
+    },
+  }
+});
