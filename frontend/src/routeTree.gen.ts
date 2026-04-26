30ae78337bcd5fe402db5a190fec6d53b1d99263
Krit Bannachaisirisuk <zestzero@Krits-MacBook-Pro.local>
2026-04-26 15:18:32 +0700

    feat(02-04): add scan route to TanStack route tree
    - Add pos.scan.tsx route import
    - Register scan route under pos parent route

--- frontend/src/routeTree.gen.ts
+++ frontend/src/routeTree.gen.ts
@@ -3,6 +3,7 @@ import { Route as indexRoute } from './routes/index'
 import { Route as loginRoute } from './routes/login'
 import { Route as posRoute } from './routes/pos'
 import { Route as posCatalogRoute } from './routes/pos.catalog'
+import { Route as posScanRoute } from './routes/pos.scan'
 import { Route as erpRoute } from './routes/erp'
 import { Route as erpIndexRoute } from './routes/erp.index'
 import { Route as erpReportsRoute } from './routes/erp.reports'
@@ -10,6 +11,6 @@ import { Route as erpReportsRoute } from './routes/erp.reports'
 export const routeTree = rootRoute.addChildren([
   indexRoute,
   loginRoute,
-  posRoute.addChildren([posCatalogRoute]),
+  posRoute.addChildren([posCatalogRoute, posScanRoute]),
   erpRoute.addChildren([erpIndexRoute, erpReportsRoute]),
 ])
